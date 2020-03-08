9fd2f754b4e35cdeadde7fa79fc74db226ddfa28
tc1 <2443505951@qq.com>
2020-03-08 23:47:29 +0800

    update2

--- file1.py
+++ file1.py
@@ -18,3 +18,47 @@ def click(event):
 text.tag_bind('link','<Enter>',show_hand_cursor)
 text.tag_bind('link','<Leave>',show_arrow_cursor)
 text.tag_bind('link','<Button-1>',click)
+
+mainloop()
+fd=tkinter.filedialog.askopenfilename()
+f=open(fd).read()
+f=f.lower()
+for i in ",.':":
+    f=f.replace(i,'')
+words=f.split()
+
+stwlist=[]
+r=open( 'stopwords.txt',encoding='utf-8').readlines()
+for i in r:
+    i=i.strip()
+    stwlist.append(i)
+
+word_ = {}
+for word in words:
+    if word.strip() not in stwlist:
+        if len(word) > 1:
+            if word != '\t':
+                if word != '\r\n':
+                    #计算词频
+                    if word in word_:
+                        word_[word] += 1
+                    else:
+                        word_[word] = 1
+
+#将词汇和词频以元组的形式保存
+word_freq = []
+for word,freq in word_.items():
+    word_freq.append((word,freq))
+
+
+#进行降序排列
+word_freq.sort(key = lambda x:x[1],reverse = True)
+
+#查看前200个结果
+for i in range(6):
+    word,freq =word_freq[i]
+    print('{0:10}{1:5}'.format(word,freq))
+
+
+textpad = Text(root,width=100,height=200)
+textpad.place(x=10,y=0)
